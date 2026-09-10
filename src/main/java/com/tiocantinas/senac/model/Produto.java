2b5dbc8fe741909b98a186fea1bffaddecf8933e
zizero19 <cicerowsdev@outlook.com>
2026-09-10 17:23:52 -0300

    feat: adicionado validações do Jakarta para a entidade Produto

--- src/main/java/com/tiocantinas/senac/model/Produto.java
+++ src/main/java/com/tiocantinas/senac/model/Produto.java
@@ -4,20 +4,25 @@ import java.math.BigDecimal;
 
 import com.tiocantinas.senac.model.enums.CategoriaProduto;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "produtos")
+@Table(name = "produto")
 @NoArgsConstructor
 @Getter
-
 @Setter
 public class Produto {
 
@@ -25,8 +30,21 @@ public class Produto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
+    @Column(nullable = false, length = 100)
     private String nome;
+
+    @NotNull
+    @PositiveOrZero
+    @Column(nullable = false, precision = 10, scale = 2)
     private BigDecimal preco;
+
+    @PositiveOrZero
+    @Column(nullable = false)
     private int quantidadeEstoque;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private CategoriaProduto categoria;
 }
